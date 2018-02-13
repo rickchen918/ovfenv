92ae758567d7b7a7f7338d47c217fe60349e35a0
rickchen918 <rickchen918@gmail.com>
2018-02-13 16:50:52 +0800

    Update remove.py

--- remove.py
+++ remove.py
@@ -3,4 +3,4 @@
 import os 
 
 # remove the python preperty for customer self-configuration 
-os.system("mv ~/ovfenv/prop.py ~/ovfenv/prop.bak")
+os.system("mv /root/ovfenv/ovfenv/prop.py /root/ovfenv/prop.bak")
